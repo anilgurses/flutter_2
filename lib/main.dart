e7f0573da0e885899ab704f72628995546027c1e
anlgrses <anilgurses98@gmail.com>
2018-07-17 09:54:25 +0300

    UI Changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.purple
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text("EasyList"),
